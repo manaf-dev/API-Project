1ce759be2b86ee3766bea030be1b40b941decdb7
Manaf <manaf18.m@gmail.com>
2024-11-05 11:57:46 +0000

    Change debug

--- backend/settings.py
+++ backend/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv(
 )
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", False)
+DEBUG = True
 
 ALLOWED_HOSTS = [".pythonanywhere.com", "127.0.0.1", "localhost"]
 
